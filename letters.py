e3a04b84dd16c34f7273ba06918556dda74b47ca
MatthiasSagerer <msagerer1@gmail.com>
2021-02-01 19:51:57 +0100

    Letters file: 'Space' and the letter 'A', 'B', 'C' & 'D'

---
+++ letters.py
@@ -0,0 +1,143 @@
+import turtle
+import math
+
+
+def space(turtle, size):
+    turtle.up()
+    turtle.forward(size/5)
+    turtle.down()
+
+def letterA(turtle, size):
+    turtle.left(75)
+    dist = size/math.cos(math.radians(15))
+    turtle.forward(dist)
+    turtle.right(150)
+    turtle.forward(dist/2)
+    turtle.right(105)
+    dist2 = size*math.tan(math.radians(15))
+    turtle.forward(dist2)
+    turtle.up()
+    turtle.backward(dist2)
+    turtle.down()
+    turtle.left(105)
+    turtle.forward(dist/2)
+    turtle.left(75)
+    turtle.up()
+    turtle.forward(size/20)
+    turtle.down()
+
+def letterB(turtle, size):
+    dist = size/5
+    dist2 = size/4
+    turtle.forward(dist)
+    turtle.circle(dist2, 180)
+    turtle.forward(dist)
+    turtle.right(180)
+    turtle.forward(dist)
+    turtle.circle(dist2, 180)
+    turtle.forward(dist)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(dist)
+    turtle.up()
+    turtle.forward(dist2+dist/4)
+    turtle.down()
+
+def letterC(turtle, size):
+    dist = size/4
+    turtle.left(90)
+    turtle.up()
+    turtle.forward(dist)
+    turtle.down()
+    turtle.right(180)
+    turtle.circle(dist, 180)
+    turtle.up()
+    turtle.forward(size/2)
+    turtle.down()
+    turtle.circle(dist, 180)
+    turtle.forward(dist*2)
+    turtle.up()
+    turtle.forward(dist)
+    turtle.left(90)
+    turtle.forward(dist*2.2)
+    turtle.down()
+
+def letterD(turtle, size):
+    dist = size/2
+    dist2 = size*3/40
+    turtle.forward(dist2)
+    turtle.circle(dist, 180)
+    turtle.forward(dist2)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.up()
+    turtle.left(90)
+    turtle.forward(dist*1.2)
+    turtle.down()
+
+def letterE(turtle, size):
+    pass
+
+def letterF(turtle, size):
+    pass
+
+def letterG(turtle, size):
+    pass
+
+def letterH(turtle, size):
+    pass
+
+def letterI(turtle, size):
+    pass
+
+def letterJ(turtle, size):
+    pass
+
+def letterK(turtle, size):
+    pass
+
+def letterL(turtle, size):
+    pass
+
+def letterM(turtle, size):
+    pass
+
+def letterN(turtle, size):
+    pass
+
+def letterO(turtle, size):
+    pass
+
+def letterP(turtle, size):
+    pass
+
+def letterQ(turtle, size):
+    pass
+
+def letterR(turtle, size):
+    pass
+
+def letterS(turtle, size):
+    pass
+
+def letterT(turtle, size):
+    pass
+
+def letterU(turtle, size):
+    pass
+
+def letterV(turtle, size):
+    pass
+
+def letterW(turtle, size):
+    pass
+
+def letterX(turtle, size):
+    pass
+
+def letterY(turtle, size):
+    pass
+
+def letterZ(turtle, size):
+    pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle
 import tkinter
 import shapes
+import letters
 import time
 
 def reset(sec):
@@ -14,14 +15,25 @@ def reset(sec):
 window = turtle.Screen()
 window.setup(width=1.0, height=1.0, startx=None, starty=None)
 ralf = turtle.Turtle()
-ralf.speed(1000)
+ralf.speed(100)
 
 
+'''
 shapes.spiralTunnel(ralf, 42, 5, 10)
 reset(3)
 shapes.polygonSpiral(ralf, 4, 2, 175, 40)
 reset(3)
 shapes.polygonSpiral(ralf, 4, 2, 175, 12)
+'''
+
+size = 100
+for i in range(2):
+    letters.letterA(ralf, size)
+    letters.letterB(ralf, size)
+    letters.letterC(ralf, size)
+    letters.letterD(ralf, size)
+    letters.space(ralf, size)
+
 
 
 tkinter.mainloop()
