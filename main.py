6bf20a2cb9e9bac586a83486eb45ba1a5ee33cd2
MatthiasSagerer <msagerer1@gmail.com>
2021-02-02 19:37:55 +0100

    letter.py : adding letter 'I' to 'Q' and the write function

--- main.py
+++ main.py
@@ -4,28 +4,42 @@ import shapes
 import letters
 import time
 
-def reset(sec):
+
+def reset(turtle, sec):
     time.sleep(sec)
-    ralf.clear()
-    ralf.penup()
-    ralf.goto(0, 0)
-    ralf.seth(0)
-    ralf.pendown()
+    turtle.clear()
+    turtle.up()
+    turtle.goto(0, 0)
+    turtle.seth(0)
+    turtle.down()
+
+def resetAndGoTo(turtle, x, y):
+    turtle.clear()
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.seth(0)
+    turtle.down()
+
 
 window = turtle.Screen()
 window.setup(width=1.0, height=1.0, startx=None, starty=None)
 ralf = turtle.Turtle()
-ralf.speed(2)
+ralf.speed(50)
 
+resetAndGoTo(ralf, -300, 0)
 
-'''
+letters.write(ralf, 150, 'Hello World')
+
+reset(ralf, 3)
+
 shapes.spiralTunnel(ralf, 42, 5, 10)
-reset(3)
-shapes.polygonSpiral(ralf, 4, 2, 175, 40)
-reset(3)
-shapes.polygonSpiral(ralf, 4, 2, 175, 12)
-'''
 
-letters.write(ralf, 100, 'oepeqereseteuevewexeyez abc')
+reset(ralf, 3)
+
+shapes.polygonSpiral(ralf, 4, 2, 175, 40)
+
+reset(ralf, 3)
+
+shapes.polygonSpiral(ralf, 4, 2, 175, 12)
 
 tkinter.mainloop()
